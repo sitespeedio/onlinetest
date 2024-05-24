a1913878f07b75676ca9051ec1e0df637f4d7130
soulgalore <peter@soulgalore.com>
2024-05-24 16:13:14 +0200

    more fix

--- test/addatest.mjs
+++ test/addatest.mjs
@@ -3,8 +3,7 @@
  * @param {import('browsertime').BrowsertimeCommands} commands
  */
 export default async function (context, commands) {
-    await commands.navigate('http://127.0.0.1:3000');
-    await commands.wait.byId('url', 5000);
+    await commands.navigate('http://127.0.0.1:3000/');
     await commands.addText.byId('https://www.wikipedia.org', 'url');
     await commands.select.selectByIdAndValue('iterations', 1); 
     await commands.mouse.singleClick.byXpath('/html/body/section/div/div[2]/ul/li[2]');
