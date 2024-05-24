8e7fac420202158d5dc946cdf9e070f82e070c8f
soulgalore <peter@soulgalore.com>
2024-05-24 07:36:39 +0200

    fix: localisation

--- server/src/util/text.js
+++ server/src/util/text.js
@@ -1,7 +1,9 @@
 import nconf from 'nconf';
 import y18n from 'y18n';
+import { getLocalFilePath } from './fileutil.js';
 const locale = nconf.get('localization:defaultLocale');
-const directory = nconf.get('localization:directory');
+const directory =
+  nconf.get('localization:directory') || getLocalFilePath('./locale');
 
 const myy18n = y18n({ updateFiles: false, locale, directory });
 
