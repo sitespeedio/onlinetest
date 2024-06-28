f64254b665e9c9901323ed54d3fcd67541182958
Peter Hedenskog <peter@soulgalore.com>
2024-06-28 08:49:06 +0200

    Navigate to the search page as a last step (#47)

--- test/scripts/addatest.mjs
+++ test/scripts/addatest.mjs
@@ -12,5 +12,6 @@ export default async function (context, commands) {
     await commands.measure.start('RunTest');
     await commands.click.byIdAndWait('submittest');
     await commands.wait.byXpath('/html/body/div[2]/h1',30000);
+    await commands.navigate('http://127.0.0.1:3000/search/');
     return commands.measure.stop();
   }
