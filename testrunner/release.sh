092a42ce81359b29fb3af3a4c1db918047cd100d
soulgalore <peter@soulgalore.com>
2024-09-24 08:33:49 +0200

    fixes

--- testrunner/release.sh
+++ testrunner/release.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
-# Super simple release script for browsertime
-# Lets use it it for now and make it better over time :)
 # You need np for this to work
 # npm install --global np
 
 npm config set tag-version-prefix=testrunner-v
-np $* --no-yarn --yolo
+np $* --no-tests
 npm config set tag-version-prefix=v
 
 # bin/browsertime.js --help > ../sitespeed.io/docs/documentation/browsertime/configuration/config.md
