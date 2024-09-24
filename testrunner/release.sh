ffe4eaa00266d76d66ba94a27cdaa74c54f136b5
soulgalore <peter@soulgalore.com>
2024-09-24 07:30:40 +0200

    release script

---
+++ testrunner/release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+# Super simple release script for browsertime
+# Lets use it it for now and make it better over time :)
+# You need np for this to work
+# npm install --global np
+
+np $* --no-yarn
+
+# bin/browsertime.js --help > ../sitespeed.io/docs/documentation/browsertime/configuration/config.md
+
+node app.js --version | tr -d '\n' > ../../sitespeed.io/docs/_includes/version/testrunner.txt
