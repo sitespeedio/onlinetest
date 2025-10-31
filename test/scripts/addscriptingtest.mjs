c47b735dff3beefd98820f822c3b8b884beceacd
Peter Hedenskog <phedenskog@wikimedia.org>
2025-10-31 11:05:47 +0100

    tune

--- test/scripts/addscriptingtest.mjs
+++ test/scripts/addscriptingtest.mjs
@@ -23,7 +23,6 @@ export default async function (context, commands) {
   await commands.measure.start('RunTest');
   await commands.click.byIdAndWait('submittest');
   // Wait for the test to finish ...
-  await commands.wait.byXpath('/html/body/div[2]/h1', 30000);
-  await commands.navigate('http://127.0.0.1:3000/search/');
+  await commands.wait.byXpathAndVisible("//h1[normalize-space(.)='Page summary']",30000);
   return commands.measure.stop();
 }
