5c9f8a67cd34914bdd31b5a0bfb1925b5b02b620
Peter Hedenskog <phedenskog@wikimedia.org>
2025-10-31 09:26:58 +0100

    Better scripting

--- test/scripts/addatest.mjs
+++ test/scripts/addatest.mjs
@@ -5,13 +5,13 @@
 export default async function (context, commands) {
     await commands.navigate('http://127.0.0.1:3000/');
     await commands.addText.byId('https://www.wikipedia.org', 'url');
-    await commands.select.selectByIdAndValue('iterations', 1); 
-    await commands.mouse.singleClick.byXpath('/html/body/section/div/div[2]/ul/li[2]');
-    await commands.wait.byId('commandlinearea', 5000);
+    await commands.select.selectByIdAndValue('iterations', 1);
+    await commands.mouse.singleClick.byXpath("//div[contains(@class,'tabs')]//span[normalize-space()='Command line args']/ancestor::a[1]");
+    await commands.wait.byIdAndVisible('commandlinearea', 5000);
     await commands.addText.byId('--headless', 'commandlinearea');
     await commands.measure.start('RunTest');
     await commands.click.byIdAndWait('submittest');
-    await commands.wait.byXpath('/html/body/div[2]/h1',30000);
-    await commands.navigate('http://127.0.0.1:3000/search/');
+    // Wait for the test to finish ...
+    await commands.wait.byXpathAndVisible("//h1[normalize-space(.)='Page summary']",30000);
     return commands.measure.stop();
   }
